0c041cd716a76bffc8c81d644b879105e3841315
NickElias01 <nicholas.elias95@gmail.com>
2023-06-18 08:44:48 -0600

    Completed Learn SQL in 60 Minutes tutorial by Web Dev Simflified

--- sqlTutorial.sql
+++ sqlTutorial.sql
@@ -54,3 +54,33 @@ WHERE name LIKE '%er%' OR band_id = 2;
 SELECT * FROM albums
 WHERE release_year = 1984 AND band_id = 1;
 
+SELECT * FROM albums
+WHERE release_year BETWEEN 2000 AND 2018;
+
+SELECT * FROM albums
+WHERE release_year IS NULL;
+
+DELETE FROM albums WHERE id = 5;
+
+SELECT * FROM albums;
+
+SELECT * FROM albums
+RIGHT JOIN bands ON bands.id = albums.band_id;
+
+SELECT SUM(release_year) FROM albums;
+
+SELECT band_id, COUNT(band_id) FROM albums
+GROUP BY band_id;
+
+SELECT b.name AS band_name, COUNT(a.id) AS num_albums
+FROM bands AS b
+LEFT JOIN albums AS a ON b.id = a.band_id
+WHERE b.name = 'Deuce'
+GROUP BY b.id
+HAVING num_albums = 1;
+
+
+
+
+
+
